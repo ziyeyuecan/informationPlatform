e62edcb9441fde0c984ce51954b1d6cf36ac6c05
周灵辉 <zhoulh001@cmrh.com>
2018-07-03 22:17:59 +0800

    [IMP] 增加轮播图

--- pages/homePage/homePage.js
+++ pages/homePage/homePage.js
@@ -4,8 +4,12 @@ const app = getApp()
 
 Page({
   data: {
+    imgUrls:['http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'],
+    autoplay: false,
+    imageMode: 'aspectFit'
   },
   onLoad: function () {
-    
   }
 })
